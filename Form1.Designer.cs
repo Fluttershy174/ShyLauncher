c7d8dbcc578098615560da5fe08ae77bfe80f820
Fluttershy174 <pavelperkov777@outlook.com>
2016-07-14 14:57:33 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.RAM = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.DebugLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.RAM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DisplayMember = "1";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Classic 1.5.2",
+            "ShyCraft 1.5.2",
+            "notepad"});
+            this.comboBox1.Location = new System.Drawing.Point(0, 98);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(284, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Выберете клиент/Select client";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // RAM
+            // 
+            this.RAM.LargeChange = 1;
+            this.RAM.Location = new System.Drawing.Point(0, 151);
+            this.RAM.Minimum = 1;
+            this.RAM.Name = "RAM";
+            this.RAM.Size = new System.Drawing.Size(188, 45);
+            this.RAM.TabIndex = 4;
+            this.RAM.Value = 1;
+            this.RAM.Scroll += new System.EventHandler(this.RAM_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "RAM 512";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Введите имя/Enter Name";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Войти/Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // DebugLabel
+            // 
+            this.DebugLabel.AutoSize = true;
+            this.DebugLabel.Location = new System.Drawing.Point(-3, 340);
+            this.DebugLabel.Name = "DebugLabel";
+            this.DebugLabel.Size = new System.Drawing.Size(65, 13);
+            this.DebugLabel.TabIndex = 8;
+            this.DebugLabel.Text = "DebugLabel";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication3.Properties.Resources.flying_derpy_vector_gif_by_xigger_d4mwb48;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 92);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 340);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(0, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(284, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Выделено более 1024 мб памяти требуется 64 битная версия Java";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 640);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DebugLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RAM);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ShyLauncher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.RAM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.ComboBox comboBox1;
+        public System.Windows.Forms.TrackBar RAM;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label DebugLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        string[] pony1 = { "YAY", "YAY!!!!", "Hello!", "20 percent cooler" }; //Pony
+        static int text1 = 0;
+        int RAMCHANGE;                                                        //RAMCHANGE int for RAMBAR
+        string client = "client name";                                        //client string for client selection
+        string name = "name";                                                          //name string for name selection
+        string appdata = Environment.GetEnvironmentVariable("APPDATA");       //appdata string for get appdata enviroment
+        string client1patch = "ShyCraft";                                     //client1patch string for get client1 patch
+        string client2patch = "minecraft";                                    //client2patch string for get client2 patch
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.DisplayMember.ToString();
+            string client = comboBox1.Text;
+            DebugLabel.Text = client;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void RAM_Scroll(object sender, EventArgs e)
+        {
+
+            int RAMCHANGE = RAM.Value *512;
+
+            label2.Text = "RAM " + RAMCHANGE.ToString();
+
+            if (RAMCHANGE >= 1536)
+            {
+                label4.Visible = true;
+            }
+            else
+            {
+                label4.Visible = false;
+            }
+
+        }
+
+        ////////relise if function and client launch////////
+        public void button1_Click_1(object sender, EventArgs e)
+        {
+            //Client1
+            if (comboBox1.Text.Equals("ShyCraft 1.5.2"))
+            {
+                string name = textBox1.Text;                //get values again
+                int RAMCHANGE = RAM.Value * 512;
+                string launch = "java -Xms" + "1024" + "m -Xmx" + RAMCHANGE + "m -cp " + appdata + "\\." + client1patch + "\\bin"+"\\*" + " -Djava.library.path=" + appdata + "\\." + client1patch + "\\" + "bin" + "\\" + "natives" + " net.minecraft.client.Minecraft " +name;
+                //DebugLabel.Text = "java -Xms" + "1024" + "m -Xmx" + RAMCHANGE + "m -cp " + appdata + "\\." + client1patch + "\\bin"+"\\*" + " -Djava.library.path=" + appdata + "\\." + client1patch + "\\" + "bin" + "\\" + "natives" + " net.minecraft.client.Minecraft " +name;
+                //.Start("java -Xms" + RAMCHANGE + "m -Xmx" + RAMCHANGE + "m -cp " + appdata + "\\." + client1patch + "\\bin" + "\\*" + " -Djava.library.path=" + appdata + "\\." + client1patch + "\\" + "bin" + "\\" + "natives" + " net.minecraft.client.Minecraft " + name);
+                //System.IO.File.WriteAllText(appdata + "writelines.bat", DebugLabel.Text);
+                //System.IO.File.WriteAllText(appdata + "writelines.txt", DebugLabel.Text);
+                Process.Start("cmd.exe", "/C" + launch);
+            }
+            //Client2
+            if (comboBox1.Text.Equals("notepad"))
+            {
+                string name = textBox1.Text;                //get values again
+                int RAMCHANGE = RAM.Value * 512;
+                DebugLabel.Text = "java pause";
+                Process.Start(@"java");
+                System.IO.File.WriteAllText(appdata + "writelines.bat", DebugLabel.Text);
+                System.IO.File.WriteAllText(appdata + "writelines.txt", DebugLabel.Text);
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.java.com/ru/download/manual.jsp");
+        }
+    }
+
+}
