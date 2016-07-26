bb278ed1f10ccff1ac6ff3faa1f8201bca65c964
Fluttershy174 <pavelperkov777@outlook.com>
2016-07-26 23:07:46 +0500

    new 1.7.10 version
    ill come to linux

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.RAM = new System.Windows.Forms.TrackBar();
@@ -38,8 +39,11 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.RAM)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -58,7 +62,8 @@
             this.comboBox1.Items.AddRange(new object[] {
             "Classic 1.5.2",
             "ShyCraft 1.5.2",
-            "notepad"});
+            "ShyCraft 1.6.4",
+            "ShyCraft 1.7.10"});
             this.comboBox1.Location = new System.Drawing.Point(0, 98);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(284, 21);
@@ -69,7 +74,7 @@
             // RAM
             // 
             this.RAM.LargeChange = 1;
-            this.RAM.Location = new System.Drawing.Point(0, 151);
+            this.RAM.Location = new System.Drawing.Point(0, 180);
             this.RAM.Minimum = 1;
             this.RAM.Name = "RAM";
             this.RAM.Size = new System.Drawing.Size(188, 45);
@@ -80,7 +85,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(188, 151);
+            this.label2.Location = new System.Drawing.Point(188, 180);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 5;
@@ -89,7 +94,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(0, 125);
+            this.textBox1.Location = new System.Drawing.Point(0, 154);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(284, 20);
             this.textBox1.TabIndex = 6;
@@ -98,7 +103,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 184);
+            this.button1.Location = new System.Drawing.Point(0, 213);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(284, 23);
             this.button1.TabIndex = 7;
@@ -118,7 +123,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WindowsFormsApplication3.Properties.Resources.flying_derpy_vector_gif_by_xigger_d4mwb48;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(2, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(284, 92);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -139,7 +144,7 @@
             // 
             this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(0, 169);
+            this.label4.Location = new System.Drawing.Point(0, 198);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(284, 12);
             this.label4.TabIndex = 10;
@@ -148,11 +153,35 @@
             this.label4.Visible = false;
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(284, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Обновить клиент/Update client";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(2, 392);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(282, 247);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 640);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.DebugLabel);
@@ -163,11 +192,13 @@
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "ShyLauncher";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.RAM)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -184,6 +215,8 @@
         private System.Windows.Forms.Label DebugLabel;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
-
+using System.Net;
+using System.IO;
 
 namespace WindowsFormsApplication3
 {
@@ -16,13 +17,37 @@ namespace WindowsFormsApplication3
     {
         string[] pony1 = { "YAY", "YAY!!!!", "Hello!", "20 percent cooler" }; //Pony
         static int text1 = 0;
-        int RAMCHANGE;                                                        //RAMCHANGE int for RAMBAR
+        int RAMCHANGE = 512;                                                  //RAMCHANGE int for RAMBAR
         string client = "client name";                                        //client string for client selection
-        string name = "name";                                                          //name string for name selection
+        string name = "name";                                                 //name string for name selection
         string appdata = Environment.GetEnvironmentVariable("APPDATA");       //appdata string for get appdata enviroment
-        string client1patch = "ShyCraft";                                     //client1patch string for get client1 patch
-        string client2patch = "minecraft";                                    //client2patch string for get client2 patch
+        string LauncherWorkFolder = Environment.GetEnvironmentVariable("APPDATA") + "\\" + "ShyLauncher";
 
+        //client1 values
+        string client1patch = "ShyCraft";                                     //client1patch string for get client1 patch
+        string client1URL = "www.client1URL.com";                             //client1URL string for get client1 URL
+        string client1update = "client1.zip";                                 //client1update string for get client1 zip
+        string client1versiontxt = "version.txt";                             //client1versiontxt string for get client1 version
+
+        //client2 values
+        string client2patch = "minecraft";                                    //client2patch string for get client2 patch
+        string client2URL = "www.client2URL.com";                             //client2URL string for get client2 zip
+        string client2update = "client2.zip";                                 //client2update string for get client2 zip
+        string client2versiontxt = "version.txt";                             //client2versiontxt string for get client2 version
+
+        //client3 values
+        string client3patch = "minecraft";                                    //client3patch string for get client3 patch
+        string client3URL = "www.client2URL.com";                             //client3URL string for get client3 zip
+        string client3update = "client2.zip";                                 //client3update string for get client3 zip
+        string client3versiontxt = "version.txt";                             //client3versiontxt string for get client3 version
+        string client3fullpatch = Environment.GetEnvironmentVariable("APPDATA") + "\\" + @".minecraft";       //appdata string for get appdata enviroment
+
+        //client3 values
+        string client4patch = "minecraft";                                    //client4patch string for get client4 patch
+        string client4URL = "www.client2URL.com";                             //client4URL string for get client4 zip
+        string client4update = "client2.zip";                                 //client4update string for get client4 zip
+        string client4versiontxt = "version.txt";                             //client4versiontxt string for get client4 version
+        string client4fullpatch = Environment.GetEnvironmentVariable("APPDATA") + "\\" + @".minecraft";       //appdata string for get appdata enviroment
 
 
 
@@ -83,35 +108,154 @@ namespace WindowsFormsApplication3
         }
 
         ////////relise if function and client launch////////
+
         public void button1_Click_1(object sender, EventArgs e)
         {
-            //Client1
+            //Client1 launch
             if (comboBox1.Text.Equals("ShyCraft 1.5.2"))
             {
                 string name = textBox1.Text;                //get values again
                 int RAMCHANGE = RAM.Value * 512;
                 string launch = "java -Xms" + "1024" + "m -Xmx" + RAMCHANGE + "m -cp " + appdata + "\\." + client1patch + "\\bin"+"\\*" + " -Djava.library.path=" + appdata + "\\." + client1patch + "\\" + "bin" + "\\" + "natives" + " net.minecraft.client.Minecraft " +name;
-                //DebugLabel.Text = "java -Xms" + "1024" + "m -Xmx" + RAMCHANGE + "m -cp " + appdata + "\\." + client1patch + "\\bin"+"\\*" + " -Djava.library.path=" + appdata + "\\." + client1patch + "\\" + "bin" + "\\" + "natives" + " net.minecraft.client.Minecraft " +name;
-                //.Start("java -Xms" + RAMCHANGE + "m -Xmx" + RAMCHANGE + "m -cp " + appdata + "\\." + client1patch + "\\bin" + "\\*" + " -Djava.library.path=" + appdata + "\\." + client1patch + "\\" + "bin" + "\\" + "natives" + " net.minecraft.client.Minecraft " + name);
-                //System.IO.File.WriteAllText(appdata + "writelines.bat", DebugLabel.Text);
-                //System.IO.File.WriteAllText(appdata + "writelines.txt", DebugLabel.Text);
                 Process.Start("cmd.exe", "/C" + launch);
             }
-            //Client2
-            if (comboBox1.Text.Equals("notepad"))
+            //Client2 launch
+            if (comboBox1.Text.Equals("Classic 1.5.2"))
             {
                 string name = textBox1.Text;                //get values again
                 int RAMCHANGE = RAM.Value * 512;
-                DebugLabel.Text = "java pause";
-                Process.Start(@"java");
-                System.IO.File.WriteAllText(appdata + "writelines.bat", DebugLabel.Text);
-                System.IO.File.WriteAllText(appdata + "writelines.txt", DebugLabel.Text);
+                string launch = "java -Xms" + "512" + "m -Xmx" + RAMCHANGE + "m -cp " + appdata + "\\." + client2patch + "\\bin" + "\\*" + " -Djava.library.path=" + appdata + "\\." + client2patch + "\\" + "bin" + "\\" + "natives" + " net.minecraft.client.Minecraft " + name;
+                Process.Start("cmd.exe", "/C" + launch);
+            }
+            //Client3 launch
+            if (comboBox1.Text.Equals("ShyCraft 1.6.4"))
+            {
+                string name = textBox1.Text;                //get values again
+                int RAMCHANGE = RAM.Value * 512;
+
+                string launch = "java.exe" + " -Xmx" + RAMCHANGE + "M" + @" -Djava.library.path=" + client3fullpatch + @"\versions\ShyCraft\natives" + @" -cp " +
+
+
+                    client3fullpatch + @"\libraries\net\minecraftforge\minecraftforge\9.11.1.1345\minecraftforge-9.11.1.1345.jar;" +
+                    client3fullpatch + @"\libraries\net\minecraft\launchwrapper\1.8\launchwrapper-1.8.jar;" +
+                    client3fullpatch + @"\libraries\org\ow2\asm\asm-all\4.1\asm-all-4.1.jar;" +
+                    client3fullpatch + @"\libraries\org\scala-lang\scala-library\2.10.2\scala-library-2.10.2.jar;" +
+                    client3fullpatch + @"\libraries\org\scala-lang\scala-compiler\2.10.2\scala-compiler-2.10.2.jar;" +
+                    client3fullpatch + @"\libraries\lzma\lzma\0.0.1\lzma-0.0.1.jar;" +
+                    client3fullpatch + @"\libraries\net\sf\jopt-simple\jopt-simple\4.5\jopt-simple-4.5.jar;" +
+                    client3fullpatch + @"\libraries\com\paulscode\codecjorbis\20101023\codecjorbis-20101023.jar;" +
+                    client3fullpatch + @"\libraries\com\paulscode\codecwav\20101023\codecwav-20101023.jar;" +
+                    client3fullpatch + @"\libraries\com\paulscode\libraryjavasound\20101123\libraryjavasound-20101123.jar;" +
+                    client3fullpatch + @"\libraries\com\paulscode\librarylwjglopenal\20100824\librarylwjglopenal-20100824.jar;" +
+                    client3fullpatch + @"\libraries\com\paulscode\soundsystem\20120107\soundsystem-20120107.jar;" +
+                    client3fullpatch + @"\libraries\argo\argo\2.25_fixed\argo-2.25_fixed.jar;" +
+                    client3fullpatch + @"\libraries\org\bouncycastle\bcprov-jdk15on\1.47\bcprov-jdk15on-1.47.jar;" +
+                    client3fullpatch + @"\libraries\com\google\guava\guava\14.0\guava-14.0.jar;" +
+                    client3fullpatch + @"\libraries\org\apache\commons\commons-lang3\3.1\commons-lang3-3.1.jar;" +
+                    client3fullpatch + @"\libraries\commons-io\commons-io\2.4\commons-io-2.4.jar;" +
+                    client3fullpatch + @"\libraries\net\java\jinput\jinput\2.0.5\jinput-2.0.5.jar;" +
+                    client3fullpatch + @"\libraries\net\java\jutils\jutils\1.0.0\jutils-1.0.0.jar;" +
+                    client3fullpatch + @"\libraries\com\google\code\gson\gson\2.2.2\gson-2.2.2.jar;" +
+                    client3fullpatch + @"\libraries\org\lwjgl\lwjgl\lwjgl\2.9.0\lwjgl-2.9.0.jar;" +
+                    client3fullpatch + @"\libraries\org\lwjgl\lwjgl\lwjgl_util\2.9.0\lwjgl_util-2.9.0.jar;" +
+                    client3fullpatch + @"\versions\ShyCraft\ShyCraft.jar" + @" -Dfml.ignoreInvalidMinecraftCertificates=true -Dfml.ignorePatchDiscrepancies=true -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy -Xmn128M net.minecraft.launchwrapper.Launch --tweakClass cpw.mods.fml.common.launcher.FMLTweaker --username " + name + @" --gameDir C:\Users\pavel\AppData\Roaming\.minecraft --assetsDir C:\Users\pavel\AppData\Roaming\.minecraft\assets";
+
+
+                Process.Start("cmd.exe", "/C" + launch);
+
+                //System.IO.File.Create(LauncherWorkFolder + @"\" + "debug.txt");
+                File.WriteAllText(appdata + @"\" + "debugFML.txt", launch);
+                //System.IO.File.Create(LauncherWorkFolder);
+                System.IO.File.WriteAllText(appdata + @"\" + "debugFML.bat", launch);
+            }
+
+            if (comboBox1.Text.Equals("ShyCraft 1.7.10"))
+            {
+                string name = textBox1.Text;                //get values again
+                int RAMCHANGE = RAM.Value * 512;
+
+                string launch = @"java.exe" + " -Xmx" + RAMCHANGE + "M" + " -Djava.library.path=" + client4fullpatch + @"\versions\ShyCraft\natives" + @" -cp " +
+                    client4fullpatch + @"\libraries\net\minecraftforge\forge\1.7.10-10.13.4.1614-1.7.10\forge-1.7.10-10.13.4.1614-1.7.10.jar;" +
+                    client4fullpatch + @"\libraries\net\minecraft\launchwrapper\1.12\launchwrapper-1.12.jar;" +
+                    client4fullpatch + @"\libraries\org\ow2\asm\asm-all\5.0.3\asm-all-5.0.3.jar;" +
+                    client4fullpatch + @"\libraries\com\typesafe\akka\akka-actor_2.11\2.3.3\akka-actor_2.11-2.3.3.jar;" +
+                    client4fullpatch + @"\libraries\com\typesafe\config\1.2.1\config-1.2.1.jar;" +
+                    client4fullpatch + @"\libraries\org\scala-lang\scala-actors-migration_2.11\1.1.0\scala-actors-migration_2.11-1.1.0.jar;" +
+                    client4fullpatch + @"\libraries\org\scala-lang\scala-compiler\2.11.1\scala-compiler-2.11.1.jar;" +
+                    client4fullpatch + @"\libraries\org\scala-lang\plugins\scala-continuations-library_2.11\1.0.2\scala-continuations-library_2.11-1.0.2.jar;" +
+                    client4fullpatch + @"\libraries\org\scala-lang\plugins\scala-continuations-plugin_2.11.1\1.0.2\scala-continuations-plugin_2.11.1-1.0.2.jar;" +
+                    client4fullpatch + @"\libraries\org\scala-lang\scala-library\2.11.1\scala-library-2.11.1.jar;" +
+                    client4fullpatch + @"\libraries\org\scala-lang\scala-parser-combinators_2.11\1.0.1\scala-parser-combinators_2.11-1.0.1.jar;" +
+                    client4fullpatch + @"\libraries\org\scala-lang\scala-reflect\2.11.1\scala-reflect-2.11.1.jar;" +
+                    client4fullpatch + @"\libraries\org\scala-lang\scala-swing_2.11\1.0.1\scala-swing_2.11-1.0.1.jar;" +
+                    client4fullpatch + @"\libraries\org\scala-lang\scala-xml_2.11\1.0.2\scala-xml_2.11-1.0.2.jar;" +
+                    client4fullpatch + @"\libraries\lzma\lzma\0.0.1\lzma-0.0.1.jar;" +
+                    client4fullpatch + @"\libraries\net\sf\jopt-simple\jopt-simple\4.5\jopt-simple-4.5.jar;" +
+                    client4fullpatch + @"\libraries\com\google\guava\guava\17.0\guava-17.0.jar;" +
+                    client4fullpatch + @"\libraries\org\apache\commons\commons-lang3\3.3.2\commons-lang3-3.3.2.jar;" +
+                    client4fullpatch + @"\libraries\com\mojang\netty\1.6\netty-1.6.jar;" +
+                    client4fullpatch + @"\libraries\com\mojang\realms\1.3.5\realms-1.3.5.jar;" +
+                    client4fullpatch + @"\libraries\org\apache\commons\commons-compress\1.8.1\commons-compress-1.8.1.jar;" +
+                    client4fullpatch + @"\libraries\org\apache\httpcomponents\httpclient\4.3.3\httpclient-4.3.3.jar;" +
+                    client4fullpatch + @"\libraries\commons-logging\commons-logging\1.1.3\commons-logging-1.1.3.jar;" +
+                    client4fullpatch + @"\libraries\org\apache\httpcomponents\httpcore\4.3.2\httpcore-4.3.2.jar;" +
+                    client4fullpatch + @"\libraries\java3d\vecmath\1.3.1\vecmath-1.3.1.jar;" +
+                    client4fullpatch + @"\libraries\net\sf\trove4j\trove4j\3.0.3\trove4j-3.0.3.jar;" +
+                    client4fullpatch + @"\libraries\com\ibm\icu\icu4j-core-mojang\51.2\icu4j-core-mojang-51.2.jar;" +
+                    client4fullpatch + @"\libraries\net\sf\jopt-simple\jopt-simple\4.5\jopt-simple-4.5.jar;" +
+                    client4fullpatch + @"\libraries\com\paulscode\codecjorbis\20101023\codecjorbis-20101023.jar;" +
+                    client4fullpatch + @"\libraries\com\paulscode\codecwav\20101023\codecwav-20101023.jar;" +
+                    client4fullpatch + @"\libraries\com\paulscode\libraryjavasound\20101123\libraryjavasound-20101123.jar;" +
+                    client4fullpatch + @"\libraries\com\paulscode\librarylwjglopenal\20100824\librarylwjglopenal-20100824.jar;" +
+                    client4fullpatch + @"\libraries\com\paulscode\soundsystem\20120107\soundsystem-20120107.jar;" +
+                    client4fullpatch + @"\libraries\io\netty\netty-all\4.0.10.Final\netty-all-4.0.10.Final.jar;" +
+                    client4fullpatch + @"\libraries\com\google\guava\guava\15.0\guava-15.0.jar;" +
+                    client4fullpatch + @"\libraries\org\apache\commons\commons-lang3\3.1\commons-lang3-3.1.jar;" +
+                    client4fullpatch + @"\libraries\commons-io\commons-io\2.4\commons-io-2.4.jar;" +
+                    client4fullpatch + @"\libraries\commons-codec\commons-codec\1.9\commons-codec-1.9.jar;" +
+                    client4fullpatch + @"\libraries\net\java\jinput\jinput\2.0.5\jinput-2.0.5.jar;" +
+                    client4fullpatch + @"\libraries\net\java\jutils\jutils\1.0.0\jutils-1.0.0.jar;" +
+                    client4fullpatch + @"\libraries\com\google\code\gson\gson\2.2.4\gson-2.2.4.jar;" +
+                    client4fullpatch + @"\libraries\com\mojang\authlib\1.5.21\authlib-1.5.21.jar;" +
+                    client4fullpatch + @"\libraries\org\apache\logging\log4j\log4j-api\2.0-beta9\log4j-api-2.0-beta9.jar;" +
+                    client4fullpatch + @"\libraries\org\apache\logging\log4j\log4j-core\2.0-beta9\log4j-core-2.0-beta9.jar;" +
+                    client4fullpatch + @"\libraries\org\lwjgl\lwjgl\lwjgl\2.9.1\lwjgl-2.9.1.jar;" +
+                    client4fullpatch + @"\libraries\org\lwjgl\lwjgl\lwjgl_util\2.9.1\lwjgl_util-2.9.1.jar;" +
+                    client4fullpatch + @"\libraries\tv\twitch\twitch\5.16\twitch-5.16.jar;" +
+                    client4fullpatch + @"\versions\ShyCraft\ShyCraft.jar -Dfml.ignoreInvalidMinecraftCertificates=true -Dfml.ignorePatchDiscrepancies=true -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy -Xmn128M net.minecraft.launchwrapper.Launch" +
+                    @" --username " + name + @" --version ShyCraft" + @" --gameDir " + client4fullpatch + @" --assetsDir " + client4fullpatch + @"\assets\ --assetIndex 1.7.10 --uuid 00000000-0000-0000-0000-000000000000 --accessToken null --userProperties [] --userType legacy --tweakClass cpw.mods.fml.common.launcher.FMLTweaker --width 925 --height 530";
+
+                pictureBox2.Visible = true;
+
+                Process.Start("cmd.exe", "/C" + launch);
+
+                //System.IO.File.Create(LauncherWorkFolder + @"\" + "debug.txt");
+                //File.WriteAllText(appdata + @"\" + "debugFML.txt", launch);
+                //System.IO.File.Create(LauncherWorkFolder);
+                //System.IO.File.WriteAllText(appdata + @"\" + "debugFML.bat", launch);
             }
         }
 
         private void label4_Click(object sender, EventArgs e)
         {
             Process.Start("https://www.java.com/ru/download/manual.jsp");
+        }
+
+        ////////download client functions////////
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.Equals("ShyCraft 1.5.2"))
+            {
+                DebugLabel.Text = LauncherWorkFolder;
+                //using (WebClient DownloadClient1 = new WebClient())
+                {
+                    //DownloadClient1.DownloadProgressChanged += wc_DownloadProgressChanged;                             //for download bar progress change
+                    //DownloadClient1.DownloadFileAsync(new System.Uri(""),
+                    //"");
+                }
+
+            }
         }
     }
 
